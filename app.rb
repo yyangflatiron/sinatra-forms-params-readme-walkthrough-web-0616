bc2dcf17f2fff08c2c2b2adfd519e1526a102eb6
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-28 11:58:09 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,6 +6,12 @@ class App < Sinatra::Base
     erb :food_form
   end
 
+  post '/food' do
+    name = params[:name]
+    food = params[:favorite_food]
+    "My name is #{name}, and I love #{food}"
+  end 
+
   # Add your post route and action below
 
 
